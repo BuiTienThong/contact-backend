52a0f71c6ce27a7f02d7f6beaa1460d62117c33e
BuiTienThong <thongb1910711@student.ctu.edu.vn>
2022-09-22 11:27:28 +0700

    Implement handlers

--- src/app.js
+++ src/app.js
@@ -8,42 +8,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({message: 'Welcome to contact book application.'});
-});
+// app.get('/', (req, res) => {
+//     res.json({message: 'Welcome to contact book application.'});
+// });
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
     });
-    app.route('/api/contacts')
-        .get(contactController.findAll)
-        .post(contactController.create)
-        .delete(contactController.deleteAll);
-    app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
-    app.route('/api/contacts/:id')
-        .get(contactController.findOne)
-        .put(contactController.update)
-        .delete(contactController.delete);
 
-        app.route('/api/contacts/:id')
-            .get(contactController.findOne)
-            .put(contactController.update)
-            .delete(contactController.delete);
-        // Handle 404 response.
-        app.use((req, res, next) => {
-        // Handler for unknown route.
-        // Call next() to pass to the error handling middleware.
-        return next(new ApiError(404, 'Resource not found'));
-        });
-        // Define error-handling middleware last, after other app.use() and routes calls.
-        app.use((err, req, res) => {
-        
-        // The centralized error handling middleware.
-        // In any route handler, calling next(error)
-        // will pass to this error handling middleware.
-        return res.status(err.statusCode || 500).json({
-            message: err.message || 'Internal Server Error',
-            });
-        });
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+// Handle 404 response.
+app.use((req, res, next) => {
+// Handler for unknown route.
+// Call next() to pass to the error handling middleware.
+    return next(new ApiError(404, 'Resource not found'));
+});
+// Define error-handling middleware last, after other app.use() and routes calls.
+app.use((err, req, res, next) => {
+
+// The centralized error handling middleware.
+// In any route handler, calling next(error)
+// will pass to this error handling middleware.
+    return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+    });
+});
 
 module.exports = app;
